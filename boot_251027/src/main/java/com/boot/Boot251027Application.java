01c363246ba5e27f6313f3c96f31dd6e93273480
yeseong1108 <ingyoeba1108@naver.com>
2025-10-27 15:43:42 +0900

    최초 커밋

---
+++ boot_251027/src/main/java/com/boot/Boot251027Application.java
@@ -0,0 +1,13 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot251027Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot251027Application.class, args);
+	}
+
+}
---
+++ boot_251027/src/test/java/com/boot/Boot251027ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot251027ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
